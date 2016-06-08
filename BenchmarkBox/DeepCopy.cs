32ec16d60b2ecd1b248797fe9206177f553b8d16
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-08 21:54:46 +0200

    Added json benchmarks.

--- BenchmarkBox/DeepCopy.cs
+++ BenchmarkBox/DeepCopy.cs
@@ -2,29 +2,44 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Runtime.Serialization;
     using System.Runtime.Serialization.Formatters.Binary;
 
     using BenchmarkDotNet.Attributes;
 
+    using Newtonsoft.Json;
+
     public class DeepCopy
     {
         private readonly Stream cachedStream;
         private readonly BinaryFormatter cachedFormatter;
+        private readonly Person[] persons;
 
         public DeepCopy()
         {
             this.cachedStream = new MemoryStream();
             this.cachedFormatter = new BinaryFormatter();
+            this.persons = Enumerable.Repeat(new Person(1, "Travis"), 1000).ToArray();
         }
 
         [Benchmark(Baseline = true)]
         public object CloneCached()
         {
+            this.cachedStream.Position = 0;
             var person = new Person(1, "Travis");
             this.cachedFormatter.Serialize(this.cachedStream, person);
             this.cachedStream.Position = 0;
             return (Person)this.cachedFormatter.Deserialize(this.cachedStream);
+        }
+
+        [Benchmark]
+        public object Clone1000Cached()
+        {
+            this.cachedStream.Position = 0;
+            this.cachedFormatter.Serialize(this.cachedStream, this.persons);
+            this.cachedStream.Position = 0;
+            return this.cachedFormatter.Deserialize(this.cachedStream);
         }
 
         [Benchmark]
@@ -36,8 +51,36 @@
             {
                 formatter.Serialize(stream, person);
                 stream.Position = 0;
-                return (Person)formatter.Deserialize(stream);
+                return formatter.Deserialize(stream);
             }
+        }
+
+        [Benchmark]
+        public object CloneJson()
+        {
+            var person = new Person(1, "Travis");
+            var serialized = JsonConvert.SerializeObject(person);
+            return JsonConvert.DeserializeObject<Person>(serialized);
+        }
+
+        [Benchmark]
+        public object Clone1000()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, this.persons);
+                stream.Position = 0;
+                return formatter.Deserialize(stream);
+            }
+        }
+
+
+        [Benchmark]
+        public object Clone1000Json()
+        {
+            var serialized = JsonConvert.SerializeObject(this.persons);
+            return JsonConvert.DeserializeObject<Person[]>(serialized);
         }
     }
 
