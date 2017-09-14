adca726491da9fe4b45f37d379fe9290ff8ea289
Johan Larsson <johan_gavle1@hotmail.com>
2017-09-15 00:42:36 +0200

    TypeBenchmarks

---
+++ BenchmarkBox/BenchmarkDotNet.Artifacts/results/TypeBenchmarks-report-github.md
@@ -0,0 +1,15 @@
+``` ini
+
+BenchmarkDotNet=v0.10.9, OS=Windows 10 Redstone 2 (10.0.15063)
+Processor=Intel Core i7-3667U CPU 2.00GHz (Ivy Bridge), ProcessorCount=4
+Frequency=2435884 Hz, Resolution=410.5286 ns, Timer=TSC
+  [Host]     : .NET Framework 4.7 (CLR 4.0.30319.42000), 64bit RyuJIT-v4.7.2102.0
+  DefaultJob : .NET Framework 4.7 (CLR 4.0.30319.42000), 64bit RyuJIT-v4.7.2102.0
+
+
+```
+ |       Method |      Mean |     Error |    StdDev | Scaled | ScaledSD |
+ |------------- |----------:|----------:|----------:|-------:|---------:|
+ |       Typeof | 1.9889 ns | 0.0975 ns | 0.0912 ns |   1.00 |     0.00 |
+ |   IntGetType | 8.1186 ns | 0.2632 ns | 0.4879 ns |   4.09 |     0.30 |
+ | ObjectEquals | 0.1406 ns | 0.1003 ns | 0.1703 ns |   0.07 |     0.08 |
--- BenchmarkBox/EqualsBenchMarks.cs
+++ BenchmarkBox/EqualsBenchMarks.cs
@@ -1,32 +1,38 @@
-﻿using BenchmarkDotNet.Attributes;
-using System.Collections.Generic;
-
-namespace BenchmarkBox
+﻿namespace BenchmarkBox
 {
-public class EqualsBenchmarks
-{
-    [Benchmark(Baseline = true)]
-    public bool Equals()
-    {
-        return 1.Equals(2);
-    }
+    using BenchmarkDotNet.Attributes;
+    using System.Collections.Generic;
 
-    [Benchmark]
-    public bool OperatorEquals()
+    public class EqualsBenchmarks
     {
-        return 1 == 2;
-    }
+        [Benchmark(Baseline = true)]
+        public bool Equals()
+        {
+            return 1.Equals(2);
+        }
 
-    [Benchmark]
-    public bool ObjectEquals()
-    {
-        return Equals(1, 2);
-    }
+        [Benchmark]
+        public bool OperatorEquals()
+        {
+            return 1 == 2;
+        }
 
-    [Benchmark]
-    public bool EqualityComparerDefaultEquals()
-    {
-        return EqualityComparer<int>.Default.Equals(1, 2);
+        [Benchmark]
+        public bool ObjectEquals()
+        {
+            return Equals(1, 2);
+        }
+
+        [Benchmark]
+        public bool EqualityComparerDefaultEquals()
+        {
+            return EqualityComparer<int>.Default.Equals(1, 2);
+        }
+
+        [Benchmark]
+        public bool ReferenceEquals()
+        {
+            return ReferenceEquals("", "");
+        }
     }
-}
 }
