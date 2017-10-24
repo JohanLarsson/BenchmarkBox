9c90d4b933368a576f3e969138d12247ff2292cf
Johan Larsson <johan_gavle1@hotmail.com>
2017-10-24 22:10:57 +0200

    EnumerableBenchmarks

---
+++ BenchmarkBox/EnumerableBenchmarks.cs
@@ -0,0 +1,23 @@
+﻿namespace BenchmarkBox
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using BenchmarkDotNet.Attributes;
+
+    public class EnumerableBenchmarks
+    {
+        private static readonly IEnumerable<int> Source = Enumerable.Range(0, 10).ToArray();
+
+        [Benchmark(Baseline = true)]
+        public object ToList()
+        {
+            return Source.ToList();
+        }
+
+        [Benchmark]
+        public object ToArray()
+        {
+            return Source.ToArray();
+        }
+    }
+}
--- BenchmarkBox/Program.cs
+++ BenchmarkBox/Program.cs
@@ -20,7 +20,7 @@
                 throw new FileNotFoundException(file);
             }
 
-            foreach (var summary in RunSingle<DictionaryBenchmarks>())
+            foreach (var summary in RunSingle<EnumerableBenchmarks>())
             {
                 CopyResult(summary.Title);
             }
