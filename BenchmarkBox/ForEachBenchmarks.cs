edb0320e6e79f3b4a13295ba7f4f770e50a7769a
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-05 21:55:34 +0100

    Amend.

--- BenchmarkBox/ForEachBenchmarks.cs
+++ BenchmarkBox/ForEachBenchmarks.cs
@@ -1,13 +1,12 @@
 ﻿namespace BenchmarkBox
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using BenchmarkDotNet.Attributes;
 
     public class ForEachBenchmarks
     {
-        private static readonly int[] Ints = Enumerable.Range(0, 1000000).ToArray();
+        private static readonly List<int> Ints = Enumerable.Range(0, 1000000).ToList();
         private static int value;
 
         [Benchmark(Baseline = true)]
@@ -21,6 +20,13 @@
         public int ToListForEach()
         {
             Ints.ToList().ForEach(x => value = x);
+            return value;
+        }
+
+        [Benchmark]
+        public int ListForEach()
+        {
+            Ints.ForEach(x => value = x);
             return value;
         }
     }
