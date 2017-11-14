4e94476df444e34208ee931058782fb5f9b7ec44
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-14 23:12:19 +0100

    EnumToStringBenchmarks

--- BenchmarkBox/Program.cs
+++ BenchmarkBox/Program.cs
@@ -20,7 +20,7 @@
                 throw new FileNotFoundException(file);
             }
 
-            foreach (var summary in RunSingle<ForEachBenchmarks>())
+            foreach (var summary in RunSingle<EnumToStringBenchmarks>())
             {
                 CopyResult(summary.Title);
             }
