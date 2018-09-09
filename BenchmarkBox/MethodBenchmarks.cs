81b6e316b0e0860917c40ff92c8e64ad8eef8185
Johan Larsson <johan_gavle1@hotmail.com>
2018-09-09 17:53:42 +0200

    CreateDelegate

--- BenchmarkBox/MethodBenchmarks.cs
+++ BenchmarkBox/MethodBenchmarks.cs
@@ -14,14 +14,19 @@ namespace BenchmarkBox
             MethodInfo);
 
 
-        [Benchmark(Baseline = true)]
+        [Benchmark]
         public int InvokeFunc() => Func();
 
-        [Benchmark]
+        [Benchmark(Baseline = true)]
         public int MethodInfoInvoke() => (int)MethodInfo.Invoke(null, null);
 
         [Benchmark]
         public int Call() => Foo();
+
+        [Benchmark]
+        public Func<int> CreateDelegate() => (Func<int>) Delegate.CreateDelegate(
+            typeof(Func<int>),
+            MethodInfo);
 
         private static int Foo() => 1;
     }
