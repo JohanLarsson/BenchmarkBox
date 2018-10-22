b5c16fb29d72e152303ec039b8b75764629ef70a
JohanLarsson <johan_gavle1@hotmail.com>
2018-10-22 10:26:58 +0200

    Tweak copy results hack.

--- BenchmarkBox/Program.cs
+++ BenchmarkBox/Program.cs
@@ -7,39 +7,36 @@
     using BenchmarkDotNet.Reports;
     using BenchmarkDotNet.Running;
 
-    public class Program
+    public static class Program
     {
         public static void Main()
         {
-            foreach (var summary in RunSingle<MethodBenchmarks>())
+            foreach (var summary in RunSingle<DistinctByBenchmarks>())
             {
                 CopyResult(summary);
             }
         }
 
-        // ReSharper disable once UnusedMember.Local
-        private static IEnumerable<Summary> RunAll()
-        {
-            var switcher = new BenchmarkSwitcher(typeof(Program).Assembly);
-            var summaries = switcher.RunAll();
-            return summaries;
-        }
+        private static IEnumerable<Summary> RunAll() => new BenchmarkSwitcher(typeof(Program).Assembly).RunAll();
 
-        private static IEnumerable<Summary> RunSingle<T>()
-        {
-            var summaries = new[] { BenchmarkRunner.Run<T>() };
-            return summaries;
-        }
+        private static IEnumerable<Summary> RunSingle<T>() => new[] { BenchmarkRunner.Run<T>() };
 
         private static void CopyResult(Summary summary)
         {
             var sourceFileName = Directory.EnumerateFiles(summary.ResultsDirectoryPath, $"*{summary.Title}-report-github.md")
                                           .Single();
-            var destinationFileName = Path.Combine(
-                summary.ResultsDirectoryPath.Split(new[] { "\\bin\\" }, StringSplitOptions.RemoveEmptyEntries).First(),
-                summary.Title.Split('.').Last() + ".md");
+            var destinationFileName = Path.ChangeExtension(FindCsFile(), ".md");
             Console.WriteLine($"Copy: {sourceFileName} -> {destinationFileName}");
             File.Copy(sourceFileName, destinationFileName, overwrite: true);
+
+            string FindCsFile()
+            {
+                return Directory.EnumerateFiles(
+                                    AppDomain.CurrentDomain.BaseDirectory.Split(new[] { "\\bin\\" }, StringSplitOptions.RemoveEmptyEntries).First(),
+                                    $"{summary.Title.Split('.').Last()}.cs",
+                                    SearchOption.AllDirectories)
+                                .Single();
+            }
         }
     }
 }
